cc8ec632999b833d43e18e65e98b10cd005703df
Oleksandr Kharutsa <al.haruca@gmail.com>
2022-11-22 14:42:45 +0100

    translate CalcBoxTotalCost component

--- src/components/CalcBoxTotalCost/CalcBoxTotalCost.tsx
+++ src/components/CalcBoxTotalCost/CalcBoxTotalCost.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { CalcBox } from "../../ui/CalcBox";
+import language from "../../framework/mock.json";
 
 type Props = {
   lotCost: string,
@@ -12,32 +13,34 @@ type Props = {
 }
 
 export const CalcBoxTotalCost: React.FC<Props> = ({ lotCost, auction_fee, totalDeliveryPrice, informService, clearance, excise, total }) => {
+  const [mock] = useState(language.polska.CalcBoxTotalCost);
+
   return (
     <div className="calculator__box calcBoxTotalCost">
       <p className="calcBoxTotalCost__title">
-        Загальна сума
+        {mock.title}
       </p>
 
       <div className="calcBoxTotalCost__form calcGrid">
         <div className="calcBoxTotalCost__resultsCont">
-          <CalcBox text="Вартість лоту + збір аукціону" value={`${Number(lotCost) + Number(auction_fee) || '0'}.00`} />
+          <CalcBox text={mock.calc__box__price__and__fee} value={`${Number(lotCost) + Number(auction_fee) || '0'}.00`} />
 
-          <CalcBox text="Вартість доставки" value={totalDeliveryPrice} />
+          <CalcBox text={mock.calc__box__delivery__price} value={totalDeliveryPrice} />
         </div>
 
         <div className="calcBoxTotalCost__resultsCont">
-          <CalcBox text="Інформаційні послуги, нетто" value={lotCost && auction_fee && totalDeliveryPrice ? `${informService}.00` : '0.00'} />
+          <CalcBox text={mock.calc__box__info__service__price} value={lotCost && auction_fee && totalDeliveryPrice ? `${informService}.00` : '0.00'} />
 
-          <CalcBox text="Розмитнення" value={clearance} />
+          <CalcBox text={mock.calc__box__clearance__price} value={clearance} />
 
-          <CalcBox text="Акциза" value={excise} />
+          <CalcBox text={mock.calc__box__excise__price} value={excise} />
         </div>
       </div>
 
       <div className="calcBoxTotalCost__footer">
         <div className="calcBoxTotalCost__footer__box">
           <p className="calcBoxTotalCost__footer__box__name">
-            Підсумкова вартість з усіма платежами
+            {mock.footer__box__title}
           </p>
 
           <p className="calcBoxTotalCost__footer__box__cost">
@@ -47,11 +50,11 @@ export const CalcBoxTotalCost: React.FC<Props> = ({ lotCost, auction_fee, totalD
 
         <div className="calcBoxTotalCost__footer__buttons">
           <button type="button" className="calcBoxTotalCost__footer__button">
-            Перевірити авто
+            {mock.footer__button__verify}
           </button>
 
           <button type="button" className="calcBoxTotalCost__footer__button">
-            КУПИТИ АВТО
+            {mock.footer__button__buy}
           </button>
         </div>
       </div>
